843ff3d5d935ff494699f8763d71005ec0702c0b
sylvie <sosylvie@hotmail.com>
2024-07-18 14:23:52 +0200

    fixed news folders

---
+++ job03.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+
+// Définition de ma fonction 
+
+function getHello(){
+    return "Hellolaplateforme!";
+}
+// Appel de ma fonction et récupération de sa valeur de retour
+
+$message = getHello();
+
+// Affichage de la valeur de retour
+
+echo $message;
+
+
+?>
---
+++ job1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>job01</title>
+</head>
+<body>
+    <p> Creation d'une fonction hello</p>
+    
+</body>
+
+
+<?php
+/*
+** Function : hello
+** Input : paramètre(s) en entrée entre () (ne pas trop en mettre)
+** Output : paramètre(s) en sortie (la fonction ne peut renvoyer qu'une seule variable ou alors un tableau).
+** Description : dire helle a la plateforme
+** Creator : sylvie
+** Date : July 16, 2024
+*/
+
+//creation de la fonction//
+
+function hello() {
+    echo "Hello laplateforme!";
+    
+}
+// appeler la fonction comme ci dessous//
+
+hello();
+
+?>
+
+</html
